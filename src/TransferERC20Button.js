8546a3a9a887a52e28fcb6cf26199e3afd816b5b
samar abdelhmeed <samar.abdelhmeed77@gmail.com>
2023-10-05 10:04:30 +0300

    add task4

--- src/TransferERC20Button.js
+++ src/TransferERC20Button.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PrimeSdk } from '@etherspot/prime-sdk';
+import { parseUnits } from '@etherspot/prime-sdk';
+
+
 
 const TransferERC20Button = () => {
-  const handleTransferERC20 = async () => {
-    const recipient = '0xD129dB5e418e389c3F7D3ae0B8771B3f76799A52';
-    const value = '0.1';
-    const tokenAddress = '0x326C977E6efc84E512bB9C30f76E30c160eD06FB';
+  const [address, setAddress] = useState('');
 
+  const handleTransferERC20 = async () => {
     try {
+      // Step 1: Get a new address (Address A)
       const primeSdk = new PrimeSdk(
-        { privateKey: process.env.REACT_APP_WALLET_PRIVATE_KEY },
-        { chainId: Number(process.env.REACT_APP_CHAIN_ID) }
+        {
+          privateKey: process.env.REACT_APP_WALLET_PRIVATE_KEY,
+          network: 'matic', // Adjust this based on your network
+        },
+        {
+          chainId: Number(process.env.REACT_APP_CHAIN_ID),
+        }
       );
 
-      console.log('address: ', primeSdk.state.walletAddress);
+      const newAddress = await primeSdk.getCounterFactualAddress();
+      setAddress(newAddress);
+
+      // Step 2: Transfer ERC20 tokens to Address A
+      const recipient = newAddress;
+      const value = primeSdk.utils.parseUnits('0.1', 18); // Adjust value and decimals as needed
+      const tokenAddress = '0x326C977E6efc84E512bB9C30f76E30c160eD06FB';
 
       const transactionData = primeSdk.contract.createTokenTransaction({
         to: tokenAddress,
         functionName: 'transfer',
-        args: [recipient, primeSdk.utils.parseUnits(value, 18)]
+        args: [recipient, value]
       });
 
       const userOp = await primeSdk.createTransaction({
@@ -26,15 +39,11 @@ const TransferERC20Button = () => {
         data: transactionData
       });
 
-      console.log('UserOp:', userOp);
-
       const uoHash = await primeSdk.sendTransaction(userOp);
 
-      console.log('UserOpHash:', uoHash);
-
-      console.log('Waiting for transaction...');
-      const txReceipt = await primeSdk.waitForTransaction(uoHash);
-      console.log('Transaction Receipt:', txReceipt);
+      // Step 3: Check the balance of Address A
+      const balance = await primeSdk.contract.readContract(tokenAddress, 'balanceOf', recipient);
+      console.log(`Balance of Address A (${recipient}):`, balance.toString());
 
     } catch (error) {
       console.error('Error transferring ERC20:', error);
@@ -42,9 +51,10 @@ const TransferERC20Button = () => {
   };
 
   return (
-    <button onClick={handleTransferERC20}>
-      Transfer ERC20
-    </button>
+    <div>
+      <button onClick={handleTransferERC20}>Transfer ERC20 to New Address</button>
+      {address && <div>Address A: {address}</div>}
+    </div>
   );
 };
 
